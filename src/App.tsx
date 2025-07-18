be9a9dd508519b1efb6e2dc1ad0227c6084674cc
Leo <leo.freakin@gmail.com>
2025-07-18 06:40:12 +0300

    Layout, Navbar And Dark,light theme

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Hero } from "./components/sections/Hero"
+import { Layout } from "./components/Layout"
+
+function App() {
+  return (
+    <Layout title="ModernDays">
+      <Hero />
+    </Layout>
+  );
+}
+
+export default App
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,5 @@
+export const Hero = () => {
+    return (
+        <section className="hero"></section>
+    );
+};
