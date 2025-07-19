80fc1cfbe1e7b90a6d00afbdb9f0647ec1f45000
Leo <leo.freakin@gmail.com>
2025-07-19 06:55:43 +0300

    Services Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "./components/sections/Hero"
 import { Layout } from "./components/Layout"
 import { Brands } from "./components/sections/Brands";
+import { Services } from "./components/sections/Services";
 
 function App() {
   return (
     <Layout title="ModernDays">
       <Hero />
       <Brands />
+      <Services />
     </Layout>
   );
 }
